9da3b62dd5fa82051cbecf50aeaf22c8e3e4d208
zhenchez <zhenouc@gmail.com>
2023-11-24 12:15:18 +0100

    finished lesson 09 mvc rest api

---
+++ 09-MVC Rest API/config/corsOptions.js
@@ -0,0 +1,18 @@
+// Cross Origin Resource Sharing
+const whitelist = [
+  "https://www.yousite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3000",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
---
+++ 09-MVC Rest API/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger, logEvents } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+
+const PORT = process.env.PORT || 3000;
+
+// custom middleware logger
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+app.use("/employees", require("./routes/api/employees"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
